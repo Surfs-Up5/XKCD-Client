074eae9d713519e9435522867e8622ccde9fbd8b
Surfs-Up5 <cgparsons5@gmail.com>
2025-10-15 15:37:24 -0400

    automatically makes requests to http server for random comics

--- XKCD-Client.go
+++ XKCD-Client.go
@@ -3,22 +3,59 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"net/http"
 	"time"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	interval := 3 * time.Second
+	interval := 5 * time.Second
 
 	ticker := time.NewTicker(interval)
 	defer ticker.Stop()
 
-	for {
-		select {
-		case <-ticker.C:
-			num := rand.Intn(100)
-			fmt.Println("Random number: ", num)
+	for range ticker.C {
+		num := rand.Intn(100)
+		fmt.Println("Checking comic: ", num)
+
+		if checkComic(num) {
+			fmt.Println("Comic already exists")
+		} else {
+			fmt.Println("Comic missing, requesting download")
+			requestDownload(num)
+
+			//loops and stalls until comic finishes downloading
+			for !checkComic(num) {
+				fmt.Println("Waiting for comic to download")
+				time.Sleep(2 * time.Second)
+			}
+			fmt.Println("Comic ", num, " downloaded")
 		}
 	}
+}
+
+func checkComic(id int) bool {
+	url := fmt.Sprintf("http://localhost:8080/comic/%d", id)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error checking comic: ", err)
+		return false
+	}
+
+	defer resp.Body.Close()
+	return resp.StatusCode == http.StatusOK
+}
+
+func requestDownload(id int) {
+	url := fmt.Sprintf("http://localhost:8080/comic/%d", id)
+	resp, err := http.Post(url, "application/json", nil)
+	if err != nil {
+		fmt.Println("Error requesting download: ", err)
+		return
+	}
+
+	defer resp.Body.Close()
+	fmt.Println("Requested download for comic: ", id)
+
 }
